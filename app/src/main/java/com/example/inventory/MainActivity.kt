e946a067d71e257aa686140a9890f7f4eaa943fd
aldodagio <103005119+aldodagio@users.noreply.github.com>
2023-12-03 19:28:29 -0800

    first commit

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.inventory.ui.theme.InventoryTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
