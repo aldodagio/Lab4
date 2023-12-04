1ab33dc106cd01817b53bad109f48649e3364114
aldodagio <103005119+aldodagio@users.noreply.github.com>
2023-12-03 19:31:20 -0800

    first commit

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 package com.example.inventory
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
